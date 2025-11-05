1af9b7fb6feb1a811c3afb9d8dcd5536069e8d08
nav2095 <aadimulamnaveenkumar.9@gmail.com>
2025-11-05 19:53:08 +0530

    Add Trivy installation script

---
+++ trivy-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install Trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+
+sudo apt-get update
+
+sudo apt-get install trivy -y
+
+echo "Trivy installed successfully"
